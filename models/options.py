74e9911abc9aa3bf9a32c072d1ea70a1caa1b78d
Yuhe Zhang <darcy.yhzhang@gmail.com>
2020-12-06 00:32:28 +0100

    update options

--- models/options.py
+++ models/options.py
@@ -2,10 +2,7 @@ import argparse
 from datetime import datetime
 import os
 class ParamOptions():
-    """This class defines options used during both training and test time.
-    It also implements several helper functions such as parsing, printing, and saving the options.
-    It also gathers additional options defined in <modify_commandline_options> functions in both dataset class and model class.
-    """
+    """This class defines options used during both training and test time."""
     def __init__(self):
         self.initialized = False
         self.time = datetime.now()
@@ -14,7 +11,7 @@ class ParamOptions():
     def initialize(self,parser):
         parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
                             help='unpaired phase reconstruction using propagation-enhanced cycle-consistent adversarial network')
-        parser.add_argument('--load_path', type=str, default = '/data/staff/tomograms/users/yuhe/CycleGAN/dataset/cyclegan_complex', help='path to training h5 files (should have a subfolder named test')
+        parser.add_argument('--load_path', type=str, default = '{self.cwd}/dataset/IMGS', help='path to training h5 files (should have a subfolder named test') # Change training path here
         parser.add_argument('--run_path', type=str, default = F'{self.cwd}/results/fig', help='path to save results')
         parser.add_argument('--run_name', type=str, default = self.time.strftime('%b%d_%H_%M'), help='folder name of this run') #TODO: modify save_path and run_name
         parser.add_argument('--batch_size', '-b', type=int, default=10, help='input batch size')
@@ -31,7 +28,7 @@ class ParamOptions():
         parser.add_argument('--num_epochs','-n', type=int, default=100, help='total number of epochs')
         parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
         parser.add_argument('--clip_max', type=float, default=1.0, help='maximum value for the gradient clipping, set to 0 if do not want to use gradient clipping.')
-        parser.add_argument('--image_stats', type=list,default=[0.8114,0.0975, 0.9675, 0.1350, 0.0653,0.2034],
+        parser.add_argument('--image_stats', type=list,default=[0,1,0,1,0,1],
                             help='statistics of training images written as [real_A_mean, real_A_std, real_B_ch1_mean, real_B_ch1_std, real_B_ch2_mean, real_B_ch2_std]')
         parser.add_argument('--energy', type = float, default=12.4, help='X-ray photon energy in keV')
         parser.add_argument('--pxs', type=float, default=1e-6, help='pixel size')
